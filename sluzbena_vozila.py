2bd3d3320a0cb6a89bc84b57b3dc7e3a9884e612
juref <jure.filipic@gmail.com>
2018-12-19 14:10:46 +0100

    dodano shranjevanje v txt datoteko

--- sluzbena_vozila.py
+++ sluzbena_vozila.py
@@ -28,11 +28,14 @@ def dodaj_avto(seznam):
                 zakljuci = True
                 return zakljuci
             else:
+
                 print ""
                 print 'Prosim izberite "da" ali "ne"!'
                 print ""
 
 def izpis_seznama(avtomobili):
+    if len(avtomobili) == 0:
+        print "Seznam je prazen!"
     for stej, a in enumerate(avtomobili):
         a.izpis_vozila(stej + 1)
 
@@ -56,7 +59,7 @@ def izbrisi_avto(seznam):
             else:
                 while True:
                     zakljuci = False
-                    del seznam[izbor - 1]
+                    del seznam[int(izbor) - 1]
                     da_ne = raw_input("Želiš izbrisati še kakšno vozilo (da/ne)? ")
 
                     if da_ne == "da":
@@ -69,9 +72,15 @@ def izbrisi_avto(seznam):
                         print 'Prosim izberite "da" ali "ne"!'
                         print ""
 
+def shrani_txt(seznam, txt_datoteka):
+    for a in seznam:
+        txt_datoteka.write(a.znamka + "," + a.model + "," + a.km + "," + a.servis + "\n")
+
 ############### Začetek aplikacije ####################
 
 print "\nDobrodošli v programu z aupravljanje službenih vozil"
+
+vozila = [] # brez tega javi error če je txt datoteka prazna!
 
 txt_datoteka = open("./_txt/vozila.txt", "r+")
 
@@ -80,13 +89,11 @@ vmesni_seznam = []
 for vrstica in txt_datoteka:
     vmesni_seznam.append(vrstica.strip())
 
-    vozila = []
-
-    for v in vmesni_seznam:
-        vozila.append(Avto(v.split(",")[0],
-                           v.split(",")[1],
-                           v.split(",")[2],
-                           v.split(",")[3]))
+for v in vmesni_seznam:
+    vozila.append(Avto(v.split(",")[0],
+                       v.split(",")[1],
+                       v.split(",")[2],
+                       v.split(",")[3]))
 
 while True:
     print ""
@@ -95,6 +102,7 @@ while True:
     print "3) Urejati datum zadnjega servisa"
     print "4) Dodati novo vozilo"
     print "5) Izbrisati obstoječe vozilo"
+    print ""
     print "q) Za prekinitev programa"
     print ""
     selection = raw_input("Prosim izberite 1, 2, 3, 4, 5 ali q: ")
@@ -111,6 +119,8 @@ while True:
     elif selection == "5":
         izbrisi_avto(vozila)
     elif selection == "q":
+        txt_data = open("./_txt/vozila.txt", "w+")
+        shrani_txt(vozila, txt_data)
         print "\nHvala in nasvidenje!"
         break
     else:
